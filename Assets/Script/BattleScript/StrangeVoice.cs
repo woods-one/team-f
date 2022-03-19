d89883dbbe7daa3c7633582c4ab1d0daf5d01939
woods-one <83874472+woods-one@users.noreply.github.com>
2022-03-19 23:07:33 +0900

    Update StrangeVoice.cs

--- Assets/Script/BattleScript/StrangeVoice.cs
+++ Assets/Script/BattleScript/StrangeVoice.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class StrangeVoice : MonoBehaviour
 {
       public Slider slider;
-      private bool maxValue;
+      private bool maxValue;//isを頭につけたほうがいいかと思われます
       private bool isClicked;
  
     void Start()
@@ -27,12 +27,12 @@ public class StrangeVoice : MonoBehaviour
         if(!isClicked) 
         {
             //5に達した場合と、0に戻った場合のフラグ切替え
-            if (slider.value == 5)
+            if (slider.value == 5)//マジックナンバーなので変数に一度代入して条件式の中に書きましょう
             {
                 maxValue = true;
             }
         
-            if (slider.value == 1)
+            if (slider.value == 1)//マジックナンバーなので変数に一度代入して条件式の中に書きましょう
             {
                 maxValue = false;
             }
@@ -40,11 +40,11 @@ public class StrangeVoice : MonoBehaviour
             //フラグによるスライダー値の増減
             if(maxValue)
             {
-                slider.value -= 0.1f;
+                slider.value -= 0.1f;//マジックナンバーなので変数に一度代入しましょう
             }
             else
             {
-                slider.value += 0.1f;
+                slider.value += 0.1f;//マジックナンバーなので変数に一度代入しましょう
             }
         }
     }
