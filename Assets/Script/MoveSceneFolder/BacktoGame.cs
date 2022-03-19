474f0536c5ee6ef821518ee96b8ef683ecbb418a
woods-one <83874472+woods-one@users.noreply.github.com>
2022-03-19 23:09:41 +0900

    Update BacktoGame.cs

--- Assets/Script/MoveSceneFolder/BacktoGame.cs
+++ Assets/Script/MoveSceneFolder/BacktoGame.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
+//toはクラス名に必要ないかと思われます
 public class BacktoGame : MonoBehaviour
 {
+    //Start、Updateメソッドは必要なければ消しましょう
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,7 @@ public class BacktoGame : MonoBehaviour
     {
         
     }
+    //謎の空白があるので消しましょう
     public void PushBack()
 	{
 		SceneManager.LoadScene ("GameScene");
